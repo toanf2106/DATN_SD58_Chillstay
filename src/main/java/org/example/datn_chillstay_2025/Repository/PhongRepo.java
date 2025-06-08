2841061c023dda5b137b2bb987c76d3b9b849636
toanf2106 <phamtoan17tb@gmail.com>
2025-06-08 23:02:52 +0700

    Revert "gộp"

--- src/main/java/org/example/datn_chillstay_2025/Repository/PhongRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.datn_chillstay_2025.Repository;
-
-import org.example.datn_chillstay_2025.Entity.Phong;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhongRepo extends JpaRepository<Phong, Integer> {
-}
